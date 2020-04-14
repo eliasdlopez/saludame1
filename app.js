e54b47ec0bf6fdeeb9166d34702d3e9b6482863d
Elias <eliasdamianlopez@gmail.com>
2020-04-13 19:01:03 -0500

    saludame2

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  if (req.query.nombre === undefined) {
+app.get('/makers/:nombre', (req, res) => {
+  if (req.params.nombre === undefined) {
     res.send('<h1>Hola desconocido!</h1>' );
   } else {
-    res.send('<h1>Hola ' + req.query.nombre + '!</h1>' );
+    res.send('<h1>Hola ' + req.params.nombre + '!</h1>' );
   };
 });
 
